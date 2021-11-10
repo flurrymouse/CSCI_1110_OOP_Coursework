2001b8f909f8d2ccc6e51da84d7fc8c8b7103265
flurrymouse <54602718+flurrymouse@users.noreply.github.com>
2021-11-09 19:52:02 -0700

    Add files via upload

---
+++ Chapter 22/Exercise22_1.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Exercise22_1 {
+  public static void main(String[] args) {
+    
+    Scanner input = new Scanner(System.in);
+    LinkedList<Character> max = new LinkedList<>();
+    LinkedList<Character> list = new LinkedList<>();
+    
+    System.out.print("Enter a string: ");
+    String string = input.nextLine(); 
+    
+    for (int i = 0; i < string.length(); i++) {
+      if (list.size() > 1 && string.charAt(i) <= list.getLast() && list.contains(string.charAt(i))) {
+      list.clear();
+      }
+      
+      list.add(string.charAt(i));
+      if (list.size() > max.size()) { 
+      max.clear();
+      max.addAll(list);
+      }
+    }
+    
+    System.out.print("The maximum consecutive substring is ");
+    
+    for (Character ch: max) { 
+    
+    System.out.print(ch); 
+    }
+    
+    System.out.println();
+    }
+  
+}
---
+++ Chapter 22/Exercise22_3.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Exercise22_3 {
+  public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+    
+      System.out.print("Enter a string s1: ");
+      String s1 = input.nextLine();
+      System.out.print("Enter a string s2: ");
+      String s2 = input.nextLine();
+    
+      int index = -1;
+      int count = 0;
+    
+      for (int i = 0; i < s1.length(); i++) {
+        if (s1.charAt(i) == s2.charAt(0) && count == 0) {
+          index = i;
+          count++;
+        }
+        else if (s1.charAt(i) == s2.charAt(count)) {
+          count++;
+        }
+        else {
+          count = 0;
+          index = -1;
+        }
+        
+        if (count == s2.length())
+          break;
+      }
+    
+      if (index > 1)
+        System.out.println("matched at index " + index);
+      else
+        System.out.println("string2 is not a substring of string1");
+    }
+  
+  
+}
